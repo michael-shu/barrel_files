a8ff7b0c49a2f4a84bcaa5ad7f2ee025bb661dcd
Michael <michaelshu34@gmail.com>
2025-06-14 19:49:24 -0400

    add comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
 import Image from "next/image";
+//Barrel
 import {NotificationToast, NavBar} from '../components';
-//import {NotificationToast, ProfileCard, SearchBar, NavBar, ImageCarousel, ModalDialog, SidebarNav, ProgressStepper, AvatarUploader, RatingStars, TabGroup, ToggleSwitch, TooltipWrapper, PaginationControls, ThemeToggler} from '../components';
-//import NotificationToast from "../components/NotificationToast";
-/*
-import ProfileCard from "../components/ProfileCard";
-import SearchBar from "../components/SearchBar";
-import NavBar from "../components/NavBar";
-import ImageCarousel from "../components/ImageCarousel";
-import ModalDialog from "../components/ModalDialog";
-import SidebarNav from "../components/SidebarNav";
-import ProgressStepper from "../components/ProgressStepper";
-import AvatarUploader from "../components/AvatarUploader";
-import RatingStars from "../components/RatingStars";
-import TabGroup from "../components/TabGroup";
-import ToggleSwitch from "../components/ToggleSwitch";
-import TooltipWrapper from "../components/TooltipWrapper";
-import PaginationControls from "../components/PaginationControls";
-import ThemeToggler from "../components/ThemeToggler";*/
 
-//import {ProfileCard, SearchBar } from '../components';
+//Direct
+//import NotificationToast from "../components/NotificationToast";
+//import NavBar from "../components/NavBar";
+
 export default function Home() {
   return (
     <>
